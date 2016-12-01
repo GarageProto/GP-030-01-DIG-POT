d744fcbd8a3cb4dfb676ffbdd38a8508e88fb296
GarageProto <MikeSBlankenship@gmail.com>
2016-12-01 09:34:10 -0800

    Update gp_max5841.ino

--- gp_max5841.ino
+++ gp_max5841.ino
@@ -12,28 +12,12 @@
  ****************************************************/
 
 /*
-===================================================================================================
-  LICENSE & DISCLAIMER
-  Copyright (C) 2014 Gabriel Staples.  All right reserved.
-   
-  ------------------------------------------------------------------------------------------------
-  License: GNU Lesser General Public License Version 3 (LGPLv3) - https://www.gnu.org/licenses/lgpl.html
-  ------------------------------------------------------------------------------------------------
-   
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
- 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU Lesser General Public License for more details.
- 
-  You should have received a copy of the GNU Lesser General Public License
-  along with this program.  If not, see http://www.gnu.org/licenses/
-===================================================================================================
+Functions :
+GPOT.setWiper(int) Writes to the Wiper register ONLY does NOT store in EEPROM
+GPOT.writeWiper()  Copies wiper register to the EEPROM
+GPOT.readWiper()   Copies whats in the EEPROM to the wiper register     
 */
+
 #include<GP_MAX5841.h>
 #include <SPI.h>
 
